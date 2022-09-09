42f18bda1e2bc21a8bf8f25a5f300af866f937f1
leonidiab <leonidiabarreto@gmail.com>
2022-09-09 20:44:55 -0300

    updating simple_random function

--- sampling.py
+++ sampling.py
@@ -14,7 +14,7 @@ def sistematic_sampling(dataset, samples):
     sistematic_sample = dataset.iloc[indexes]
     return sistematic_sample
 
-def grouping_sampling(dataset, group_quiantity):
+def grouping_sampling(dataset, group_quiantity, label_name): #label_name must be a string value
     interval = len(dataset)//group_quiantity
     groups = []
     group_id = 0
@@ -26,8 +26,14 @@ def grouping_sampling(dataset, group_quiantity):
             count = 0
             group_id += 1
             
-    dataset['groups'] = groups #you can change the label name
+    dataset[label_name] = groups
     random.seed(1)
     selected_group = random.randint(0, group_quiantity)
-    return dataset[dataset['groups']==selected_group]
+    return dataset[dataset[label_name]==selected_group]
 
+def stratified_sampling(dataset,label_name,testSize=100/len(dataset)): #label_name must be a string
+    split = StratifiedShuffleSplit(test_size=testSize)
+    for x,y in split.split(dataset,dataset[label_name]):
+        df_x = dataset.iloc[x]
+        df_y = dataset.iloc[y]
+    return (df_x, df_y)
